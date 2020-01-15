aebe4d345275214396700724d7d8d74e9e494a16
John Liu <johnliu478@gmail.com>
2020-01-15 09:15:05 -0500

    Implemented all 3 given testcases to be used for p0

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_DIR'] = os.path.expanduser('~/ece250_data')
-
+# directory for where temporary files will be placed
+app.config['UPLOAD_DIR'] = os.path.expanduser('~')
 
 @app.route('/')
 def index():
@@ -52,44 +52,53 @@ def upload_files():
             'compilefail.html',
             error=err)
 
-    # feed test case into program
-    test_case_in = subprocess.Popen(
-        ['cat', os.path.join('testcases', 'p0', 'test01.in')],
-        universal_newlines=True,
-        stdout=subprocess.PIPE)
-    test_case_in.wait()
-    test_process = subprocess.Popen(
-        './a.out',
-        shell=True,
-        cwd=temp_dir,
-        stdin=test_case_in.stdout,
-        universal_newlines=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    test_process.wait()
-
-    # compare program output to test case output
-    expected_output = []
-    actual_output = []
-    success = True
-    with open(os.path.join('testcases', 'p0', 'test01.out')) as test_case_out:
-        # iterate through each line in test case and program output
+    # iterate through each test case and compare with program output
+    test_case_data = []
+    test_case_files = [('test1.in', 'test1.out'), ('test2.in', 'test2.out'), ('test3.in', 'test3.out')]
+    test_case_num = 1
+    all_passed = True
+    for test_case_in_file, test_case_out_file in test_case_files:
+        # pipe testcase to program
+        test_case_in = subprocess.Popen(
+            ['cat', os.path.join('testcases', 'p0', test_case_in_file)],
+            universal_newlines=True,
+            stdout=subprocess.PIPE)
+        test_case_in.wait()
+        test_process = subprocess.Popen(
+            './playlistdriver',
+            shell=True,
+            cwd=temp_dir,
+            stdin=test_case_in.stdout,
+            universal_newlines=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        test_process.wait()
         prog_output = test_process.stdout
-        while True:
-            test_case_line = test_case_out.readline()
-            prog_output_line = prog_output.readline()
-            if test_case_line == '' and prog_output_line == '':
-                break
-            if test_case_line != prog_output_line:
-                success = False
-            expected_output.append(test_case_line)
-            actual_output.append(prog_output_line)
+
+        expected_output_lines = []
+        actual_output_lines = []
+        success = True
+        with open(os.path.join('testcases', 'p0', test_case_out_file)) as test_case_out:
+            # compare test case output and program output line by line
+            while True:
+                test_case_line = test_case_out.readline()
+                prog_output_line = prog_output.readline()
+                if test_case_line == '' and prog_output_line == '':
+                    break
+                if test_case_line != prog_output_line:
+                    success = False
+                    all_passed = False
+                expected_output_lines.append(test_case_line)
+                actual_output_lines.append(prog_output_line)
+        test_case_data.append((test_case_num, expected_output_lines, actual_output_lines, success))
+        test_case_num += 1
+
+    print(test_case_data)
 
     # remove temp dir and its contents
     shutil.rmtree(temp_dir)
 
     return render_template(
-        'testcase.html',
-        expected=expected_output,
-        actual=actual_output,
-        success=success)
+        'testcases.html',
+        test_cases=test_case_data,
+        all_passed=all_passed)
