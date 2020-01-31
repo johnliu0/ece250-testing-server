23287e63e1d885cab34df94d7c7a0463b5e4223b
John Liu <johnliu478@gmail.com>
2020-01-31 14:38:40 -0500

    Fix email validation sending wrong link

--- app/emails.py
+++ app/emails.py
@@ -4,11 +4,12 @@ from flask import current_app as flask_app
 # from sendgrid.helpers.mail import Mail
 
 def send_validation_email(user):
-    domain_name = flask_app.config['MAIL_DOMAIN_NAME']
+    domain_name = flask_app.config['DOMAIN_NAME']
+    mail_domain_name = flask_app.config['MAIL_DOMAIN_NAME']
     # use https for production and http for development
     verif_link_prefix = 'http' + ('' if flask_app.env == 'development' else 's') + '://'
     verif_link = f'{verif_link_prefix}{domain_name}/auth/verify/{user.validation_token}'
-    mailgun_url_param = 'sandboxa8fe96c682af4667a52b6bb5e581298f.mailgun.org' if flask_app.env == 'development' else domain_name
+    mailgun_url_param = 'sandboxa8fe96c682af4667a52b6bb5e581298f.mailgun.org' if flask_app.env == 'development' else mail_domain_name
 
     return requests.post(
         f'https://api.mailgun.net/v3/{mailgun_url_param}/messages',
